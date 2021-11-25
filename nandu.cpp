9965a8f49866263c44ae3b5470598ebb29201565
nandini <93575437+nandhini532@users.noreply.github.com>
2021-11-25 14:48:24 +0530

    Add files via upload

---
+++ nandu.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int p,t,r,si,ci;
+	p=100,t=2,r=12;
+	si=p*t*r/100;
+	printf("simple interest:%d\n",si);
+	ci=p*(1+r/100)*t-1;
+	printf("compound interest:%d\n",ci);
+}
