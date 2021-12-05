a087e43f3a023a6714182a34d22eb9e97534669c
nandini <93575437+nandhini532@users.noreply.github.com>
2021-12-05 17:46:16 +0530

    Add files via upload

---
+++ a.out.cpp
@@ -0,0 +1,14 @@
+
+@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,add;
+  printf("\nEnter the value of a: ");
+  scanf("%d",&a);
+  printf("\nEnter the value of b: ");
+  scanf("%d",&b);
+  add=a+b;
+  printf("\nAddition of two numbers is:%d",add);
+} 
+
