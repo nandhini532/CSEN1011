014c8e1863323f73368d8214197ca2aff266c4ee
nandini <93575437+nandhini532@users.noreply.github.com>
2021-12-05 17:25:06 +0530

    Add files via upload

---
+++ evenorodd.c
@@ -0,0 +1,35 @@
+
+/*ALGORITHM TO FIND A NUMBER IS EVEN OR ODD
+Step-1:Start
+Step-2:Accept one number
+Step-3:Divide the number with 2,if the remainder is 0 it is even else it is odd
+Step-4:Stop*/
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? a;
+
+   raptor_prompt_variable_zzyz ="Enter a  number: ";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> a;
+   if (a % 2==0)
+   {
+      cout << "The number is EVEN" << endl;   }
+   else
+   {
+      cout << "The number is ODD" << endl;   }
+
+   return 0;
+}
+
+
+/*Learning
+1.learned to find whether a number is even or odd
+2.if else is used in the program
+
