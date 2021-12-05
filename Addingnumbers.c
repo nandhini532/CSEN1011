62e96c426a2deed61a61683f83268f9bead56e77
nandini <93575437+nandhini532@users.noreply.github.com>
2021-12-05 17:21:57 +0530

    Add files via upload

---
+++ Addingnumbers.c
@@ -0,0 +1,26 @@
+
+/*ALGORITHM TO ADD TWO NUMBERS
+Step-1:Start
+Step-2:Enter a number a
+Step-3:Enter a number b
+Step-4:Perform addition of the two numbers (add=a+b)
+Step-5:Display the result
+Step-6:Stop*/
+
+
+#include<stdio.h>
+int main()
+{
+	int a,b,add;
+	printf("\nEnter the value of a: ");
+	scanf("%d",&a);
+	printf("\nEnter the value of b: ");
+	scanf("%d",&b);
+	add=a+b;
+	printf("\nAddition of two numbers is:%d",add);
+}
+
+
+/*Learnings
+1.A simple program to add two numbers using addition operation */
+
