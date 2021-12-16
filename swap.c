d4becac0f64438890efc5241fc2486a6ca7a73d8
nandini <93575437+nandhini532@users.noreply.github.com>
2021-12-16 19:43:27 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,29 @@
+/*
+Algorithm:
+1. Start.
+2. Accept two numbers a and b.
+3.Take another temp variable.
+4. temp=a,a=b,and b=temp.
+5. Stop.
+*/
+#include<stdio.h>
+int main()
+{
+  int a=10,b=20;
+  printf("Before swap a=%d b=%d\n",a,b);
+  a=a+b; //a=30 (10+20)
+  b=a-b; //b=10 (30-20)
+  a=a-b; //a=20 (30-10)
+  printf("After swap a=%d b=%d\n",a,b);
+  return 0;
+}
+/*
+
+Output:
+Before swap 
+a=10 b=20
+After swap 
+a=20 b=10
+Learnings:
+1. swapping without temporary variable.
+*/
