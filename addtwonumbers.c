269457305aa8a5b632e1c22d90712fa38fb4c3c1
nandini <93575437+nandhini532@users.noreply.github.com>
2021-12-29 19:12:21 +0530

    Create addtwonumbers.c

---
+++ addtwonumbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+        printf("Enter two integers:\n");
+	scanf("%d %d\n",&a,&b);
+	sum=a+b;
+	printf("%d",sum);
+	return 0;
+}
+/*
+Output:
+Enter two integers
+5 7
+12
+*/
