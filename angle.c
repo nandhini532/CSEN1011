cb2a0d08f1a6bbf23cfb398bb8c63b8f68f6897a
nandini <93575437+nandhini532@users.noreply.github.com>
2021-12-29 19:13:22 +0530

    Create angle.c

---
+++ angle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a1,a2,a3;
+        printf("Enter two angles:\n");
+	scanf("%d %d\n",&a1,&a2);
+	a3=180-a1-a2;
+	printf("%d",a3);
+	return 0;
+}
+/*
+Output:
+Enter two angles
+60 60
+120
+*/
