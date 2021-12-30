c1fa6981e4796faff641213afce2ed3ac7f83317
nandini <93575437+nandhini532@users.noreply.github.com>
2021-12-30 15:06:01 +0000

    ascii value

---
+++ ascii.c
@@ -0,0 +1,21 @@
+/*ALGORITHM TO FIND ASCII VALUE OF A CHARACTER
+Step-1:Start
+step-2:Print Enter any character
+step-4:Enter any character
+step-5:Print the ASCII value of the character
+step-6:Stop*/
+
+#include<stdio.h>
+int main()
+{
+  char ch;
+  printf("\nEnter any character: ");
+  scanf("%c",&ch);
+  printf("ASCII value is:%d",ch);
+}
+
+/*Output:
+Enter any character:E
+ASCII value is:69
+Learnings:
+learned to find the ascii value of characters*/
