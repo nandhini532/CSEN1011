bb5dd66d5eee5be64bfcfcc492006d7242524e08
nandhini532 <93575437+nandhini532@users.noreply.github.com>
2022-02-23 23:18:41 +0530

    Create productrecur.c

---
+++ productrecur.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int product(int a,int b){
+  if(a<b){
+    return product(b,a);
+  }
+  else if(b!=0){
+    return(a+product(a,b-1));
+  }
+  else{
+    return 0;
+  }
+}
+int main(){
+  int a,b,result;
+  scanf("%d %d",&a,&b);
+  result=product(a,b);
+  printf("product=%d",result);
+  return 0;
+}
+© 2022 GitHub, Inc.
