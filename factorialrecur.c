247e6463c24bcd576af399c42fbf6afa4e5d9a31
nandhini532 <93575437+nandhini532@users.noreply.github.com>
2022-02-23 23:13:52 +0530

    Create factorialrecur.c

---
+++ factorialrecur.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+long int fact(int n){
+  if(n>=1){
+    return n*fact(n-1);
+  }
+  else{
+    return 1;
+  }
+}
+int main(){
+  int n;
+  scanf("%d",&n);
+  printf("factorial=%ld",fact(n));
+  return 0;
+}
+© 2022 GitHub, Inc.
+Terms
