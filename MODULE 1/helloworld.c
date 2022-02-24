197fb33b711c91f5e6c8d4665585120eca9723c0
nandhini532 <93575437+nandhini532@users.noreply.github.com>
2022-02-24 12:06:25 +0530

    Create helloworld.c

---
+++ MODULE 1/helloworld.c
@@ -0,0 +1,15 @@
+/*ALGORITHM FOR HELLO WORLD
+Step-1:Start
+Step-2:Print "Hello World"
+Step-3:Stop  */
+
+
+#include<stdio.h>
+int main()
+{
+printf("Hello World");
+}
+
+
+/*Learnings
+1.Learned algorithm of a simple program Hello World  */
