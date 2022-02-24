eb6e2c2c7971b4725819ee2d7d0b61b020156fdc
nandhini532 <93575437+nandhini532@users.noreply.github.com>
2022-02-24 15:57:38 +0530

    Create customer.c

---
+++ customer.c
@@ -0,0 +1,13 @@
+Step-1:Start
+Step-2:Enter the name of 1stcustomer, c1.name
+Step-3:Enter the amount of 1stcustomer, c1.amount
+Step-4:Enter the number of 1stcustomer, c1.number
+Step-5:Enter the name of 2ndcustomer, c2.name
+Step-6:Enter the amount of 2ndcustomer, c2.amount
+Step-7:Enter the number of 2ndcustomer, c2.number
+Step-8:for(i=1;i<=10;i++)
+Step-9:If the number of digits in mobile number of two customers is less than 10 then print "Invalid Mobile Number"
+Step-10:If the mobile number does not contain 10 digits then print "Invalid Entry"
+Step-11:If the amount paid by the customers is negative then print "Invalid Amount" else print amount
+Step-12:Total amount=a1.amount+a2.amount
+Step-13:Stop
